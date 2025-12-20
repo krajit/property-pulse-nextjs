7939bab855e74e9131fc2dbaaa9ea6248efc0cb1
Ajit Kumar <ajitkumar22@gmail.com>
2025-12-21 00:40:04 +0530

    vid 12 - params promise

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,24 +1,9 @@
-'use client';
+const  PropertyPage = async ({ params }) => {
 
-import { useRouter, useParams, useSearchParams, usePathname } from "next/navigation";
-
-const  PropertyPage = () => {
-
-    const router = useRouter();
-    const params = useParams();
-    const searchParams = useSearchParams();
-    const pathName = usePathname();
-
-    console.log(router);
+    const { id } = await params;
 
     return ( <div>
-        <button onClick={ () => router.replace('/')}> Go HOme</button>
-
-        <p>Property Page: {params.id}</p>
-
-        <p>Hello: {searchParams.get('fName')} {searchParams.get('lName')}</p>
-
-        <p>{pathName}</p>
+        Property page: { id }
     </div> );
 }
  
