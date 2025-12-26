852d85ae49d9a566a4b60a3e529c4ab8a558ee15
Ajit Kumar <ajitkumar22@gmail.com>
2025-12-27 00:55:17 +0530

    video 19- done

---
+++ app/loading.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const LoadingPage = () => {
+  return (
+    <ClipLoader color="#3b82f6 cssOverride={override} size={150} aria-label='loading-spinner'" />
+  );
+};
+
+export default LoadingPage;
