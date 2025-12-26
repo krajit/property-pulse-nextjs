38a6d082071e7316eec102bd3449258645c868ce
Ajit Kumar <ajitkumar22@gmail.com>
2025-12-26 23:36:53 +0530

    video 18 done

---
+++ components/HomeProperties.jsx
@@ -0,0 +1,42 @@
+import properties from "@/data/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+import Link from "next/link";
+
+const HomeProperties = () => {
+  const recentProperties = properties.slice(0, 3);
+  return (
+    <>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto py-6 px-4">
+          <h2 className="text-3xl font-bold text-center mb-6 text-blue-500">
+            Recent Properties
+          </h2>
+
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {recentProperties.map((property) => (
+                <PropertyCard
+                  key={property._id}
+                  property={property}
+                ></PropertyCard>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section className="m-auto max-w-lg px-6">
+        <Link
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+          href="/properties"
+        >
+          View All Properties
+        </Link>
+      </section>
+    </>
+  );
+};
+
+export default HomeProperties;
