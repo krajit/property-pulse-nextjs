4c5f4d47d0047481a6210ce8363d2ac794bc8bd7
Ajit Kumar <ajitkumar22@gmail.com>
2025-12-26 22:56:24 +0530

    done video 18

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,16 +1,24 @@
-"use client";
-
-import { useRouter, useParams } from "next/navigation";
+import properties from "@/data/properties.json";
+import PropertyCard from "@/components/PropertyCard";
 
 const PropertiesPage = () => {
-  const router = useRouter();
-  const params = useParams();
-  console.log(router);
   return (
-    <div>
-      <p>Property {params.id}</p>
-      <button onClick={() => router.replace("/")}>Go Home</button>
-    </div>
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto py-6 px-4">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property) => (
+              <PropertyCard
+                key={property._id}
+                property={property}
+              ></PropertyCard>
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
   );
 };
 
