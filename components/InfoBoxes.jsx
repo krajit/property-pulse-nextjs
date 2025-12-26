6b3184291c43c613b3416a7009f23e725e351991
Ajit Kumar <ajitkumar22@gmail.com>
2025-12-26 16:37:51 +0530

    manual info box created

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -1,5 +1,40 @@
+import Link from "next/link";
+
 const InfoBoxes = () => {
-  return <div>Info Boxes</div>;
+  return (
+    <section>
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold">For Renters</h2>
+            <p className="mt-2 mb-4">
+              Find your dream rental property. Bookmark properties and contact
+              owners.
+            </p>
+            <Link
+              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+              href="/properties"
+            >
+              Browse Properties
+            </Link>
+          </div>
+          <div className="bg-blue-100 p-6 rounded-lg shadow-md">
+            <h2 className="text-2xl font-bold">For Property Owners</h2>
+            <p className="mt-2 mb-4">
+              List your properties and reach potential tenants. Rent as an
+              airbnb or long term.
+            </p>
+            <Link
+              className="inline-block bg-blue-500 text-white rounded-lg px-4 py-2 hover:bg-blue-600"
+              href="/properties/add"
+            >
+              Add Property
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default InfoBoxes;
