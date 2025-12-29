6cba285e383e1497f314d76219e0cbd06f54382f
Ajit Kumar <ajitkumar22@gmail.com>
2025-12-29 20:28:49 +0530

    video 26 done

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,5 +1,8 @@
+import PropertyDetails from "@/components/PropertyDetails";
 import connectDB from "@/config/database";
+
 import Property from "@/models/Property";
+
 import Link from "next/link";
 
 import { FaArrowLeft } from "react-icons/fa";
@@ -31,8 +34,7 @@ const PropertyPage = async ({ params }) => {
       <section className="bg-blue-50">
         <div className="container m-auto py-10 px-6">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            {/* prop info */}
-            content
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
